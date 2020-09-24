86d579846c71addb6f1944bc7fa6ce2a8ebd047d
Elymas-Magus <41921067+Elymas-Magus@users.noreply.github.com>
2020-09-24 05:35:26 -0300

    Update bd.sql

--- bd.sql
+++ bd.sql
@@ -1,45 +1,50 @@
-CREATE TABLE PRODUCTS
-(
-  	id int(11) NOT NULL AUTO_INCREMENT,
-  	name varchar(500) NOT NULL,
-  	price decimal(18,2) NOT NULL,
-  	city int(11) NOT NULL,
-  	description varchar(500) DEFAULT NULL,
-  	created datetime NOT NULL,
-  	modified datetime DEFAULT NULL,
-  	CONSTRAINT prodpk PRIMARY KEY (id),
-  	CONSTRAINT prodfk FOREIGN KEY (city) REFERENCES CITYS (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 CREATE TABLE CITYS
 (
-  	id int(11) NOT NULL,
-  	nome varchar(500) NOT NULL,
-  	uf varchar(5) NOT NULL,
-  	total_hab int(11) DEFAULT NULL,
-  	created datetime NOT NULL,
-  	modified datetime DEFAULT NULL,
-  	CONSTRAINT citypk PRIMARY KEY (id)
+    id int(11) NOT NULL,
+    nome varchar(500) NOT NULL,
+    uf varchar(5) NOT NULL,
+    total_hab int(11) DEFAULT NULL,
+    created datetime NOT NULL,
+    modified datetime DEFAULT NULL,
+    CONSTRAINT citypk PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE users (
-	  id int(11) NOT NULL,
-	  name varchar(100) NOT NULL,
-	  email varchar(200) NOT NULL,
-	  pwd varchar(500) NOT NULL,
-	  access_lvl int(11) NOT NULL,
-	  created datetime NOT NULL,
-	  modified datetime DEFAULT NULL,
-	  CONSTRAINT userpk PRIMARY KEY (id),
-	  CONSTRAINT userfk FOREIGN KEY (access_lvl) REFERENCES access_lvl (id);
+CREATE TABLE PRODUCTS
+(
+    id int(11) NOT NULL AUTO_INCREMENT,
+    name varchar(500) NOT NULL,
+    price decimal(18,2) NOT NULL,
+    amount int(11) NOT NULL,
+    city int(11) NOT NULL,
+    description varchar(500) DEFAULT NULL,
+    created datetime NOT NULL,
+    modified datetime DEFAULT NULL,
+    CONSTRAINT prodpk PRIMARY KEY (id),
+    CONSTRAINT prodfk FOREIGN KEY (city) REFERENCES CITYS (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE ACCESS_LVL (
+    id int(11) NOT NULL,
+    nome varchar(100) NOT NULL,
+    descricao varchar(500) NOT NULL,
+    CONSTRAINT access_pk PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE access_lvl (
-	  id int(11) NOT NULL,
-	  nome varchar(100) NOT NULL,
-	  descricao varchar(500) NOT NULL,
-	  CONSTRAINT access_pk PRIMARY KEY (id)
+
+CREATE TABLE USERS (
+    id int(11) NOT NULL,
+    name varchar(100) NOT NULL,
+    email varchar(200) NOT NULL,
+    pwd varchar(500) NOT NULL,
+    access_lvl int(11) NOT NULL,
+    created datetime NOT NULL,
+    modified datetime DEFAULT NULL,
+    CONSTRAINT userpk PRIMARY KEY (id),
+    CONSTRAINT userfk FOREIGN KEY (access_lvl) REFERENCES access_lvl (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
 
 
 INSERT INTO access_lvl VALUES
@@ -64,4 +69,4 @@ INSERT INTO PRODUCTS VALUES
 (4, 'Cadeira de rodinha', '2.00', 2, 2, '0', '2020-09-19 09:05:18', NULL),
 (6, 'Camiseta', '30.00', 1000, 3, '0', '2020-09-16 07:59:51', NULL),
 (7, 'Caneta', '1.00', 1000, 1, '0', '2020-09-16 08:07:17', NULL),
-(8, 'Lapis', '1.00', 1000, 1, '0', '2020-09-16 08:11:04', NULL),
+(8, 'Lapis', '1.00', 1000, 1, '0', '2020-09-16 08:11:04', NULL);
